a4838b939083612436db20984e7ee703c7457720
dataartist-og <79862447+dataartist-og@users.noreply.github.com>
2024-06-26 02:03:10 -0700

    Update core_recipe.py to use the blank pages mask
    Fix for mismatched page -> image pipeline due to removal of textless pages by the parser.

--- papermage/recipes/core_recipe.py
+++ papermage/recipes/core_recipe.py
@@ -65,8 +65,8 @@ VILA_LABELS_MAP = {
     "Bibliography": BibliographiesFieldName,
     "Equation": EquationsFieldName,
     "Algorithm": AlgorithmsFieldName,
-    # "Figure": FiguresFieldName,
-    # "Table": TablesFieldName,
+    "Figure": FiguresFieldName,
+    "Table": TablesFieldName,
     "Caption": CaptionsFieldName,
     "Header": HeadersFieldName,
     "Footer": FootersFieldName,
@@ -89,29 +89,36 @@ class CoreRecipe(Recipe):
         self.parser = PDFPlumberParser()
         self.rasterizer = PDF2ImageRasterizer()
 
-        # with warnings.catch_warnings():
-        #     warnings.simplefilter("ignore")
-        #     self.word_predictor = SVMWordPredictor.from_path(svm_word_predictor_path)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            self.word_predictor = SVMWordPredictor.from_path(svm_word_predictor_path)
 
         self.publaynet_block_predictor = LPEffDetPubLayNetBlockPredictor.from_pretrained()
         self.ivila_predictor = IVILATokenClassificationPredictor.from_pretrained(ivila_predictor_path)
+        self.bio_roberta_predictor = HFBIOTaggerPredictor.from_pretrained(
+            bio_roberta_predictor_path,
+            entity_name="tokens",
+            context_name="pages",
+        )
         self.sent_predictor = PysbdSentencePredictor()
         self.logger.info("Finished instantiating recipe")
 
     def from_pdf(self, pdf: Path) -> Document:
         self.logger.info("Parsing document...")
-        doc = self.parser.parse(input_pdf_path=pdf)
+        doc, is_blank = self.parser.parse(input_pdf_path=pdf, return_blank_page_mask=True)
 
         self.logger.info("Rasterizing document...")
         images = self.rasterizer.rasterize(input_pdf_path=pdf, dpi=self.dpi)
+        images = [im for im, blank in zip(images, is_blank) if not blank]
+        
         doc.annotate_images(images=list(images))
         self.rasterizer.attach_images(images=images, doc=doc)
         return self.from_doc(doc=doc)
 
     def from_doc(self, doc: Document) -> Document:
-        # self.logger.info("Predicting words...")
-        # words = self.word_predictor.predict(doc=doc)
-        # doc.annotate_layer(name=WordsFieldName, entities=words)
+        self.logger.info("Predicting words...")
+        words = self.word_predictor.predict(doc=doc)
+        doc.annotate_layer(name=WordsFieldName, entities=words)
 
         self.logger.info("Predicting sentences...")
         sentences = self.sent_predictor.predict(doc=doc)
@@ -123,20 +130,7 @@ class CoreRecipe(Recipe):
             blocks = self.publaynet_block_predictor.predict(doc=doc)
         doc.annotate_layer(name=BlocksFieldName, entities=blocks)
 
-        self.logger.info("Predicting figures and tables...")
-        figures = []
-        tables = []
-        for block in blocks:
-            if block.metadata.type == "Figure":
-                figure = Entity(boxes=block.boxes)
-                figures.append(figure)
-            elif block.metadata.type == "Table":
-                table = Entity(boxes=block.boxes)
-                tables.append(table)
-        doc.annotate_layer(name=FiguresFieldName, entities=figures)
-        doc.annotate_layer(name=TablesFieldName, entities=tables)
-
-        # self.logger.info("Predicting vila...")
+        self.logger.info("Predicting vila...")
         vila_entities = self.ivila_predictor.predict(doc=doc)
         doc.annotate_layer(name="vila_entities", entities=vila_entities)
 
@@ -146,7 +140,7 @@ class CoreRecipe(Recipe):
                     [b for t in doc.intersect_by_span(entity, name=TokensFieldName) for b in t.boxes]
                 )
             ]
-            # entity.text = make_text(entity=entity, document=doc)
+            entity.text = make_text(entity=entity, document=doc)
         preds = group_by(entities=vila_entities, metadata_field="label", metadata_values_map=VILA_LABELS_MAP)
         doc.annotate(*preds)
         return doc
