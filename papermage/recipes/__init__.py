4b6ab422bfa23c0875a81381306fe119f40ac619
dataartist-og <79862447+dataartist-og@users.noreply.github.com>
2024-08-06 20:37:33 -0700

    updating requirements.txt

--- papermage/recipes/__init__.py
+++ papermage/recipes/__init__.py
@@ -1,7 +1,9 @@
 from papermage.recipes.core_recipe import CoreRecipe
 from papermage.recipes.text_recipe import TextRecipe
+from papermage.recipes.sci_recipe import SciRecipe
 
 __all__ = [
     "CoreRecipe",
     "TextRecipe",
+    "SciRecipe"
 ]
